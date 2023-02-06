11f2dca1be560d9a5371f154acdbd3e397e9e5d0
Rasmus Jessen <113165703+Rasmus2103@users.noreply.github.com>
2023-02-06 13:22:09 +0000

    Looking good

---
+++ fix-samples.sh
@@ -0,0 +1,8 @@
+cd $/workspaces/bbs-Rasmus2103/samples
+ mv animals/countries/ .
+ mv countries/movies/ .
+ mv animals/Monkies.txt animals/Monkeys.txt 
+ echo Monkeys > animals/Monkeys.txt 
+ mv movies/Germany.txt countries/
+ mv countries/.apes.txt animals/
+ cd -
